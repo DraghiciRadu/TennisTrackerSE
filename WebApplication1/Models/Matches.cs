83ede0153b86ce53edc196420eb03588b6057be0
PAUL <PAUL@DESKTOP-5QKIV2G>
2025-03-31 22:19:43 +0300

    Add project files.

---
+++ WebApplication1/Models/Matches.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class Matches : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
