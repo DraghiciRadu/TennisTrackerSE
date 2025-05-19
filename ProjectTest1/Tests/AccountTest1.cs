cde2392c3b06be733af82257b6563d2f726848bb
DraghiciRadu <draghici.radu.q4f@student.ucv.ro>
2025-05-19 21:29:10 +0300

    Admin Unit Testing (#6)
    Co-authored-by: PAUL <PAUL@DESKTOP-5QKIV2G>

---
+++ ProjectTest1/Tests/AccountTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+    namespace ProjectTest1.Tests;
+
+
+    [TestClass]
+    public class AccountTest1
+    {
+
+        private UserManager<ApplicationUser> _userManager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+            services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            _userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        }
+
+        [TestMethod]
+        public async Task AdminTestAsync()
+        {
+            // Arrange
+            var testEmail = "admin@tennis.com";
+            var testPassword = "Admin@123";
+
+            var adminEmail = "admin@tennis.com";
+            var adminPassword = "Admin@123";
+            var user = new ApplicationUser { UserName = testEmail, Email = testEmail };
+
+            // Act
+            var createResult = await _userManager.CreateAsync(user, testPassword);
+
+            // Assert
+            var resultUser = await _userManager.FindByEmailAsync(testEmail);
+            var resultPassword = await _userManager.CheckPasswordAsync(resultUser,adminPassword);
+
+            Assert.AreEqual(adminEmail, resultUser?.Email);
+            Assert.IsTrue(resultPassword);
+        }
+    }
